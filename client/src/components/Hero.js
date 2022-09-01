598012b24259f425fa1604a152be75c4f1b64bc3
Geronimo <vulcandro@hotmail.com>
2022-09-01 11:09:25 -0500

    Adding new gif

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -12,22 +12,29 @@ const Hero = () => {
     const [provider, setProvider] = useState();
 
     return(
-        <div className="row bg-black py-5">
-            <div className="col-md-2"></div>
-            <div className="col-md-4">
-                <div className="container">
-                    <div className="row w-50 mx-auto pb-5 d-flex justify-content-around">
-                        <a href="https://discord.com" className="social-media" id="twitter"> </a>
-                        <a href="https://discord.com" className="social-media" id="discord"> </a>
-                        <a href="https://discord.com" className="social-media" id="youtube"> </a>
-                        <a href="https://discord.com" className="social-media" id="instagram"> </a>
+        <div className="row bg-black">
+            <div className="col-md-5 pb-2">
+                <div className="container muy-left">
+                    <div className="row mx-auto pb-4 d-flex justify-content-around social-media-container">
+                        <div className="col">
+                            <a href="https://discord.com" className="social-media" id="twitter"> </a>
+                        </div>
+                        <div className="col">
+                            <a href="https://discord.com" className="social-media" id="discord"> </a>
+                        </div>
+                        <div className="col">
+                            <a href="https://discord.com" className="social-media" id="youtube"> </a>
+                        </div>
+                        <div className="col">
+                            <a href="https://discord.com" className="social-media" id="instagram"> </a>
+                        </div>
                     </div>
                     <div className="container w-75 pb-5">
                         <img src={MuyLogo} alt="Logo Muy Muñoz 23" id='muyLogo' className='mx-auto'/>
                     </div>
                     { account?
                         <>
-                            <button className="btn-wallet p-1" onClick={
+                            <button className="btn-wallet p-1 text-uppercase font-weight-bold" onClick={
                                 async ()=>{
                                 await web3modal.clearCachedProvider();
                                 window.localStorage.clear();
@@ -36,7 +43,7 @@ const Hero = () => {
                                 }>Disconnect</button>
                             </>:
                         <>
-                            <button className="btn-wallet p-1" onClick={
+                            <button className="btn-wallet p-1 text-uppercase font-weight-bold" onClick={
                                 () => {
                                 handleConnectWallet({
                                     setAccount,
@@ -49,7 +56,7 @@ const Hero = () => {
                     }
                 </div>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-5">
                 <img src={require('../assets/images/change5.gif').default} alt="Carrusel NFT" className="mx-auto flyman" />
             </div>
             <div className="col-md-2"></div>
