3fbb7d0d75f2352409b41129532923e6d35bd509
Sorpresa <gerorubio@outlook.com>
2022-10-05 12:42:36 -0500

    Fix to deploy in vercel

--- client/src/components/WhiteListForm.js
+++ client/src/components/WhiteListForm.js
@@ -8,7 +8,6 @@ const WhiteListForm = ({state, changeWhitelist}) => {
     const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
     const SHEET_ID = process.env.REACT_APP_SHEET_ID;
     const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
-    const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace();
 
     const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
